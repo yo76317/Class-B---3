fde4d09dfdb6e8edc3355557492a4264bfe5c023
yo76317 <yo76317@gmail.com>
2022-01-17 10:58:10 +0800

    後台院線片清單列表

--- back/movie.php
+++ back/movie.php
@@ -3,21 +3,28 @@
 <div style="display:flex;width:100%">
 <div>
 <hr>
-<div style="display:flex;width:100%">
-<div>
-    <img src="" alt="">
+
+<div style="overflow:auto;height:420px;">
+<?php
+$mos=$Movie->all(" ORDER BY rank");
+foreach ($mos as $key => $movie) {
+?>
+<div style="display:flex;width:100%;">
+<div style="width:10%">
+    <img src="img/<?=$movie['poster'];?>" style="width:100%">
 </div>
-<div>
+
+<div style="width:10%">
     分級:
-    <img src="icon/" alt="">
+    <img src="icon/<?=$movie['level'];?>.png">
 </div>
-<div>
+<div style="width:80%">
     <div style="display:flex">
-        <div style="width:33%">片名:</div>
-        <div style="width:33%">片長:</div>
-        <div style="width:33%">上映時間:</div>
+        <div style="width:33%">片名:<?=$movie['name'];?></div>
+        <div style="width:33%">片長:<?=$movie['length'];?></div>
+        <div style="width:33%">上映時間:<?=$movie['ondate'];?></div>
     </div>
-    <div>
+    <div style="text-align:right">
         <button>顯示</button>
         <button>往上</button>
         <button>往下</button>
@@ -25,7 +32,12 @@
         <button>刪除電影</button>
     </div>
     <div>
-        劇情介紹：
+        劇情介紹：<?=$movie['intro'];?>
     </div>
 </div>
 </div> 
+</div>
+<hr>
+<?php
+}
+?>
