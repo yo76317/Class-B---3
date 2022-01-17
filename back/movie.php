aaed95b4e8a7dff6a2fd048cde5ed37b6d3216b8
yo76317 <yo76317@gmail.com>
2022-01-17 10:41:51 +0800

    刻大概院線片管理頁面

--- back/movie.php
+++ back/movie.php
@@ -1,1 +1,28 @@
-院線片 
+<button>新增電影</button>
+<hr>
+<div style="display:flex;width:100%">
+<div>
+    <img src="" alt="">
+</div>
+<div>
+    分級:
+    <img src="icon/" alt="">
+</div>
+<div>
+    <div style="display:flex">
+        <div style="width:33%">片名:</div>
+        <div style="width:33%">片長:</div>
+        <div style="width:33%">上映時間:</div>
+    </div>
+    <div>
+        <button>顯示</button>
+        <button>往上</button>
+        <button>往下</button>
+        <button>編輯電影</button>
+        <button>刪除電影</button>
+    </div>
+    <div>
+        劇情介紹：
+    </div>
+</div>
+</div> 
