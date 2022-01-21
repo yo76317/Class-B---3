fca69264f1594e8d20d9d5aeab4b3bd487961482
yo76317 <yo76317@gmail.com>
2022-01-21 16:37:52 +0800

    過日場次不能訂票

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -3,7 +3,16 @@ include_once "../base.php";
 $movie=$Movie->find($_GET['id']);
 $date=$_GET['date'];
 
-for($i=1;$i<=5;$i++){
+if($date!=date("Y-m-d") || date("G")<14){
+    $s=5;
+}else{
+    $s=5-ceil((date("G")-13)/2);
+}
+
+
+for($i=(6-$s);$i<=5;$i++){
+
+    
     $seats=$Ord->math('sum','qt',['movie'=>$movie['name'],'date'=>$date,'session'=>$ss[$i]]);
     echo "<option value='$i'>{$ss[$i]} 剩餘座位 ".(20-$seats)."</option>";
 } 
